3f4ab82f810f7d849c99ee7d0b67d5b3be2a9737
Joseph Benitez Arroyo <benitezarroyojoseph@gmail.com>
2024-11-27 13:45:35 -0400

    Inicializando la libreria

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import typescript from "rollup-plugin-typescript2";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/index.tsx", // Entrada principal de la librería
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss({
+      extract: true, // Genera un archivo CSS separado
+      minimize: true,
+      sourceMap: true,
+    }),
+    terser(), // Minifica el código
+  ],
+};
